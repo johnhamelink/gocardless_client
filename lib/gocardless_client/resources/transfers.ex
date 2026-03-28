d18be4c8b5c26d0b04746512d4fe12b6742f50f7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-28 23:03:08 +0530

    GoCardlessClient.Resources.Transfers

---
+++ lib/gocardless_client/resources/transfers.ex
@@ -0,0 +1,54 @@
+defmodule GoCardlessClient.Resources.Transfers do
+  @moduledoc """
+  GoCardlessClient Transfers API.
+
+  See https://developer.gocardless.com/api-reference/#transfers for full documentation.
+  """
+
+  alias GoCardlessClient.{Client, Paginator, Resource}
+
+  @resource_key "transfers"
+  @base_path "/transfers"
+
+  @doc "Creates a new transfers resource."
+  @spec create(Client.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def create(%Client{} = client, params, opts \\ []) do
+    Resource.post(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Retrieves a single transfers by ID."
+  @spec get(Client.t(), String.t(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def get(%Client{} = client, id, opts \\ []) do
+    Resource.get(client, "#{@base_path}/#{id}", @resource_key, opts)
+  end
+
+  @doc "Updates a transfers."
+  @spec update(Client.t(), String.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def update(%Client{} = client, id, params, opts \\ []) do
+    Resource.put(client, "#{@base_path}/#{id}", @resource_key, params, opts)
+  end
+
+  @doc "Lists transfers with optional filter params."
+  @spec list(Client.t(), map(), keyword()) ::
+          {:ok, %{items: [map()], meta: map()}}
+          | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def list(%Client{} = client, params \\ %{}, opts \\ []) do
+    Resource.list(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Returns a lazy `Stream` over all pages of transfers."
+  @spec stream(Client.t(), map(), keyword()) :: Enumerable.t()
+  def stream(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.stream(client, @base_path, params, @resource_key, opts)
+  end
+
+  @doc "Eagerly collects all transfers into a list across all pages."
+  @spec collect_all(Client.t(), map(), keyword()) ::
+          {:ok, [map()]} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def collect_all(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.collect(client, @base_path, params, @resource_key, opts)
+  end
+end
