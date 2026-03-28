dd961f024369040594b98cb801c64a106eebbdae
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-28 22:53:55 +0530

    GoCardlessClient.Resources.BillingRequestFlows

---
+++ lib/gocardless_client/resources/billing_request_flows.ex
@@ -0,0 +1,61 @@
+defmodule GoCardlessClient.Resources.BillingRequestFlows do
+  @moduledoc """
+  GoCardlessClient Billing Request Flows API.
+
+  See https://developer.gocardless.com/api-reference/#billing-request-flows for full documentation.
+  """
+
+  alias GoCardlessClient.{Client, Paginator, Resource}
+
+  @resource_key "billing_request_flows"
+  @base_path "/billing_request_flows"
+
+  @doc "Creates a new billing request flows resource."
+  @spec create(Client.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def create(%Client{} = client, params, opts \\ []) do
+    Resource.post(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Retrieves a single billing request flows by ID."
+  @spec get(Client.t(), String.t(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def get(%Client{} = client, id, opts \\ []) do
+    Resource.get(client, "#{@base_path}/#{id}", @resource_key, opts)
+  end
+
+  @doc "Updates a billing request flows."
+  @spec update(Client.t(), String.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def update(%Client{} = client, id, params, opts \\ []) do
+    Resource.put(client, "#{@base_path}/#{id}", @resource_key, params, opts)
+  end
+
+  @doc "Lists billing_request_flows with optional filter params."
+  @spec list(Client.t(), map(), keyword()) ::
+          {:ok, %{items: [map()], meta: map()}}
+          | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def list(%Client{} = client, params \\ %{}, opts \\ []) do
+    Resource.list(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Returns a lazy `Stream` over all pages of billing_request_flows."
+  @spec stream(Client.t(), map(), keyword()) :: Enumerable.t()
+  def stream(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.stream(client, @base_path, params, @resource_key, opts)
+  end
+
+  @doc "Eagerly collects all billing_request_flows into a list across all pages."
+  @spec collect_all(Client.t(), map(), keyword()) ::
+          {:ok, [map()]} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def collect_all(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.collect(client, @base_path, params, @resource_key, opts)
+  end
+
+  @doc "Initialises a billing request flow."
+  @spec initialise(Client.t(), String.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def initialise(%Client{} = client, id, params \\ %{}, opts \\ []) do
+    Resource.action(client, "#{@base_path}/#{id}", "initialise", @resource_key, params, opts)
+  end
+end
