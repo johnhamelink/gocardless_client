e6c144a6c32e456f464bee115368c81489c4cd19
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-28 23:02:58 +0530

    GoCardlessClient.Resources.TaxRates

---
+++ lib/gocardless_client/resources/tax_rates.ex
@@ -0,0 +1,54 @@
+defmodule GoCardlessClient.Resources.TaxRates do
+  @moduledoc """
+  GoCardlessClient Tax Rates API.
+
+  See https://developer.gocardless.com/api-reference/#tax-rates for full documentation.
+  """
+
+  alias GoCardlessClient.{Client, Paginator, Resource}
+
+  @resource_key "tax_rates"
+  @base_path "/tax_rates"
+
+  @doc "Creates a new tax rates resource."
+  @spec create(Client.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def create(%Client{} = client, params, opts \\ []) do
+    Resource.post(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Retrieves a single tax rates by ID."
+  @spec get(Client.t(), String.t(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def get(%Client{} = client, id, opts \\ []) do
+    Resource.get(client, "#{@base_path}/#{id}", @resource_key, opts)
+  end
+
+  @doc "Updates a tax rates."
+  @spec update(Client.t(), String.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def update(%Client{} = client, id, params, opts \\ []) do
+    Resource.put(client, "#{@base_path}/#{id}", @resource_key, params, opts)
+  end
+
+  @doc "Lists tax_rates with optional filter params."
+  @spec list(Client.t(), map(), keyword()) ::
+          {:ok, %{items: [map()], meta: map()}}
+          | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def list(%Client{} = client, params \\ %{}, opts \\ []) do
+    Resource.list(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Returns a lazy `Stream` over all pages of tax_rates."
+  @spec stream(Client.t(), map(), keyword()) :: Enumerable.t()
+  def stream(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.stream(client, @base_path, params, @resource_key, opts)
+  end
+
+  @doc "Eagerly collects all tax_rates into a list across all pages."
+  @spec collect_all(Client.t(), map(), keyword()) ::
+          {:ok, [map()]} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def collect_all(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.collect(client, @base_path, params, @resource_key, opts)
+  end
+end
