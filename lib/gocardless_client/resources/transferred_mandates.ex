33a042fc578189e911c7f9b57b218e027d74610f
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-28 23:03:03 +0530

    GoCardlessClient.Resources.TransferredMandates

---
+++ lib/gocardless_client/resources/transferred_mandates.ex
@@ -0,0 +1,54 @@
+defmodule GoCardlessClient.Resources.TransferredMandates do
+  @moduledoc """
+  GoCardlessClient Transferred Mandates API.
+
+  See https://developer.gocardless.com/api-reference/#transferred-mandates for full documentation.
+  """
+
+  alias GoCardlessClient.{Client, Paginator, Resource}
+
+  @resource_key "transferred_mandates"
+  @base_path "/transferred_mandates"
+
+  @doc "Creates a new transferred mandates resource."
+  @spec create(Client.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def create(%Client{} = client, params, opts \\ []) do
+    Resource.post(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Retrieves a single transferred mandates by ID."
+  @spec get(Client.t(), String.t(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def get(%Client{} = client, id, opts \\ []) do
+    Resource.get(client, "#{@base_path}/#{id}", @resource_key, opts)
+  end
+
+  @doc "Updates a transferred mandates."
+  @spec update(Client.t(), String.t(), map(), keyword()) ::
+          {:ok, map()} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def update(%Client{} = client, id, params, opts \\ []) do
+    Resource.put(client, "#{@base_path}/#{id}", @resource_key, params, opts)
+  end
+
+  @doc "Lists transferred_mandates with optional filter params."
+  @spec list(Client.t(), map(), keyword()) ::
+          {:ok, %{items: [map()], meta: map()}}
+          | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def list(%Client{} = client, params \\ %{}, opts \\ []) do
+    Resource.list(client, @base_path, @resource_key, params, opts)
+  end
+
+  @doc "Returns a lazy `Stream` over all pages of transferred_mandates."
+  @spec stream(Client.t(), map(), keyword()) :: Enumerable.t()
+  def stream(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.stream(client, @base_path, params, @resource_key, opts)
+  end
+
+  @doc "Eagerly collects all transferred_mandates into a list across all pages."
+  @spec collect_all(Client.t(), map(), keyword()) ::
+          {:ok, [map()]} | {:error, GoCardlessClient.APIError.t() | GoCardlessClient.Error.t()}
+  def collect_all(%Client{} = client, params \\ %{}, opts \\ []) do
+    Paginator.collect(client, @base_path, params, @resource_key, opts)
+  end
+end
